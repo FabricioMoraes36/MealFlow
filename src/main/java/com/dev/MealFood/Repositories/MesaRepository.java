88594f19266a1ce2a1cc06902c2b2169d8fbb73c
Guilherme Souza <guilhermematos2313@gmail.com>
2025-11-06 13:42:11 -0300

    feat: Criaçao de DTOs e Service

---
+++ src/main/java/com/dev/MealFood/Repositories/MesaRepository.java
@@ -0,0 +1,8 @@
+package com.dev.MealFood.Repositories;
+
+import com.dev.MealFood.Models.Mesa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MesaRepository extends JpaRepository <Mesa, Long> {
+
+}
