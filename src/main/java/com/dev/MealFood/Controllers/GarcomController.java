682eb8611eeeec2c331ef7fda516e9f135ec8cca
Matheus Oliveira <thhheusoliveira@gmail.com>
2025-11-06 13:43:11 -0300

    Implementado Regras de Negócio do Garçom

--- src/main/java/com/dev/MealFood/Controllers/GarcomController.java
+++ src/main/java/com/dev/MealFood/Controllers/GarcomController.java
@@ -1,4 +1,41 @@
 package com.dev.MealFood.Controllers;
 
+import com.dev.MealFood.Models.Garcom;
+import com.dev.MealFood.Services.GarcomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/garcons")
 public class GarcomController {
+
+    private final GarcomService service;
+
+    public GarcomController(GarcomService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Garcom criar(@RequestBody Garcom garcom) {
+        return service.criar(garcom);
+    }
+
+    @GetMapping
+    public List<Garcom> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Garcom buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PatchMapping("/{garcomId}/mesas/{mesaId}")
+    public Garcom atribuirMesa(
+            @PathVariable Long garcomId,
+            @PathVariable Long mesaId
+    ) {
+        return service.atribuirMesa(garcomId, mesaId);
+    }
 }
--- src/main/java/com/dev/MealFood/Services/GarcomService.java
+++ src/main/java/com/dev/MealFood/Services/GarcomService.java
@@ -1,4 +1,56 @@
 package com.dev.MealFood.Services;
 
+
+import com.dev.MealFood.Models.Garcom;
+import com.dev.MealFood.Repositories.GarcomRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GarcomService {
+
+    private final GarcomRepository repository;
+
+    public GarcomService(GarcomRepository repository) {
+        this.repository = repository;
+    }
+
+    public Garcom criar(Garcom garcom) {
+        return repository.save(garcom);
+    }
+
+    public List<Garcom> listar() {
+        return repository.findAll();
+    }
+
+    public Garcom buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Garçom não encontrado!"));
+    }
+
+    @Transactional
+    public Garcom atribuirMesa(Long garcomId, Long mesaId) {
+
+        Garcom garcom = buscarPorId(garcomId);
+        Mesa mesa = mesaRepository.findById(mesaId)
+                .orElseThrow(() -> new RuntimeException("Mesa não encontrada"));
+
+        // Regra - Garçom só pode ter até 5 mesas >:c
+        if (garcom.getMesas().size() >= 5) {
+            throw new RuntimeException("Esse garçom já atende o máximo de 5 mesas");
+        }
+
+        // Regra - Garçom deve ter o mesmo Turno da Mesa
+        if (!mesa.getTurno().equals(garcom.getTurno())) {
+            throw new RuntimeException("Garçom só pode atender mesas do mesmo Turno");
+        }
+
+        mesa.setGarcom(garcom);
+        garcom.getMesas().add(mesa);
+
+        mesaRepository.save(mesa);
+        return repository.save(garcom);
+    }
 }
