dd270b410c58910fbe7ad38991f949730d08a2c2
Fabricio Moraes <fabricio36365@outlook.com>
2025-11-06 13:36:26 -0300

    Fix: prato arrumado

--- src/main/java/com/dev/MealFood/Models/Prato.java
+++ src/main/java/com/dev/MealFood/Models/Prato.java
@@ -1,17 +1,34 @@
 package com.dev.MealFood.Models;
 
 import com.dev.MealFood.Enums.PratoCategoria;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
 public class Prato {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "prato_nome", nullable = false)
     private String nome;
-    private String preco;
+
+    @Column(name = "prato_preco", nullable = false)
+    private BigDecimal preco;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "prato_categoria", nullable = false)
     private PratoCategoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+            name = "prato_ingrediente",
+            joinColumns = @JoinColumn(name = "prato_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingrediente_id")
+    )
     private List<Ingredientes> ingredientes;
 }
