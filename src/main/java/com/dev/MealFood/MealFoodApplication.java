fa64227ea43cb96cc5448977ae239a70f7a0d5f5
Fabricio Moraes <fabricio36365@outlook.com>
2025-11-06 12:33:58 -0300

    First Commit

---
+++ src/main/java/com/dev/MealFood/MealFoodApplication.java
@@ -0,0 +1,13 @@
+package com.dev.MealFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealFoodApplication.class, args);
+	}
+
+}
