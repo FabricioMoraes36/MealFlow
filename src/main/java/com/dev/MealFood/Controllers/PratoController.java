2ff661b1ba5b6dd1ae66c93f7d66eda9706fdc47
Fabricio Moraes <fabricio36365@outlook.com>
2025-11-06 13:23:39 -0300

    Feat: cria a entidade ingrediente para complementar a entidade prato e seu relacionamento

---
+++ src/main/java/com/dev/MealFood/Controllers/PratoController.java
@@ -0,0 +1,7 @@
+package com.dev.MealFood.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PratoController {
+}
--- src/main/java/com/dev/MealFood/Models/Prato.java
+++ src/main/java/com/dev/MealFood/Models/Prato.java
@@ -1,17 +1,43 @@
 package com.dev.MealFood.Models;
 
 import com.dev.MealFood.Enums.PratoCategoria;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "pratos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Prato {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "prato_nome")
     private String nome;
-    private String preco;
+
+    @Column(name = "prato_preco")
+    private BigDecimal preco;
+
+    @Column(name = "prato_categoria")
+    @Enumerated(EnumType.STRING)
     private PratoCategoria categoria;
+
+
+    @ManyToOne
+    @JoinTable(name = "prato_ingrediente",
+    joinColumns = @JoinColumn(name = "prato_id"),
+    inverseJoinColumns = @JoinColumn(name = "ingrediente_id"))
     private List<Ingredientes> ingredientes;
+
 }
